1aa181b015e5274428a1f55d115d967db90260e2
Celox16 <sck04184@naver.com>
2021-07-29 16:30:26 +0900

    feat: add judge func

--- Client/SinglePlayForm.Designer.cs
+++ Client/SinglePlayForm.Designer.cs
@@ -30,6 +30,9 @@ namespace Client
         private void InitializeComponent()
         {
             this.boardPicture = new System.Windows.Forms.PictureBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.boardPicture)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,21 +47,56 @@ namespace Client
             this.boardPicture.Paint += new System.Windows.Forms.PaintEventHandler(this.boardPicture_Paint);
             this.boardPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boardPicture_MouseDown);
             // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(639, 20);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(100, 40);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "GameStart";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(557, 79);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(272, 15);
+            this.status.TabIndex = 3;
+            this.status.Text = "Start to Game!!";
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // SinglePlayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(864, 541);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playButton);
             this.Controls.Add(this.boardPicture);
             this.Name = "SinglePlayForm";
             this.Text = "SinglePlayForm";
             ((System.ComponentModel.ISupportInitialize)(this.boardPicture)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox boardPicture;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status;
     }
 }
--- Client/SinglePlayForm.cs
+++ Client/SinglePlayForm.cs
@@ -18,9 +18,44 @@ namespace Client
         private enum Horse { none = 0, BLACK, WHITE}; // 흰색 말 = 1, 검은 말 = 2, 비어있으면 0으로 설정
         private Horse[,] board = new Horse[edgeCount, edgeCount]; // 15*15형태의 2차원 배열형태를 만들어줌 board의 각 원소는 enum자료형 (흰색, 검은색말)
         private Horse nowPlayer = Horse.BLACK; // 현재 게임을 진행하는 플레이어를 정해줌 (검은돌이 먼저 수행함)
+
+        private bool playing = false;
         public SinglePlayForm()
         {
             InitializeComponent();
+        }
+
+        private bool judge() // 승리 판정 함수
+        {
+            for (int i = 0; i < edgeCount - 4; i++) // 가로
+                for (int j = 0; j < edgeCount; j++)
+                    if (board[i, j] == nowPlayer && board[i + 1, j] == nowPlayer && board[i + 2, j] == nowPlayer &&
+                        board[i + 3, j] == nowPlayer && board[i + 4, j] == nowPlayer)
+                        return true;
+            for (int i = 0; i < edgeCount; i++) // 세로
+                for (int j = 4; j < edgeCount; j++)
+                    if (board[i, j] == nowPlayer && board[i, j - 1] == nowPlayer && board[i, j - 2] == nowPlayer &&
+                        board[i, j - 3] == nowPlayer && board[i, j - 4] == nowPlayer)
+                        return true;
+            for (int i = 0; i < edgeCount - 4; i++) // Y = X 직선
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if (board[i, j] == nowPlayer && board[i + 1, j + 1] == nowPlayer && board[i + 2, j + 2] == nowPlayer &&
+                        board[i + 3, j + 3] == nowPlayer && board[i + 4, j + 4] == nowPlayer)
+                        return true;
+            for (int i = 4; i < edgeCount; i++) // Y = -X 직선
+                for (int j = 0; j < edgeCount - 4; j++)
+                    if (board[i, j] == nowPlayer && board[i - 1, j + 1] == nowPlayer && board[i - 2, j + 2] == nowPlayer &&
+                        board[i - 3, j + 3] == nowPlayer && board[i - 4, j + 4] == nowPlayer)
+                        return true;
+            return false;
+        }
+
+        private void refresh() // 게임이 끝난 후 모든 돌을 refresh함 (사라짐)
+        {
+            this.boardPicture.Refresh();
+            for (int i = 0; i < edgeCount; i++)
+                for (int j = 0; j < edgeCount; j++)
+                    board[i, j] = Horse.none;
         }
 
         private void boardPicture_MouseDown(object sender, MouseEventArgs e) // 오목판을 클릭했을때의 이벤트
@@ -33,7 +68,12 @@ namespace Client
                 MessageBox.Show("테두리를 벗어날 수 없습니다.");
                 return;
             }
-            MessageBox.Show(x + ", " + y); // 현재 클릭한 좌표를 보여줌
+
+            if (board[x, y] != Horse.none) return;
+            board[x, y] = nowPlayer;
+
+            //MessageBox.Show(x + ", " + y); // 현재 클릭한 좌표를 보여줌
+
             if(nowPlayer == Horse.BLACK) // 만약 사용자가 black이라면
             {
                 SolidBrush brush = new SolidBrush(Color.Black); // 현재 오목판의 그래픽 객체에
@@ -44,6 +84,18 @@ namespace Client
             {
                 SolidBrush brush = new SolidBrush(Color.White); // 현재 오목판의 그래픽 객체에
                 g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize); // 원형을 그리게 됨
+            }
+
+            if (judge())
+            {
+                status.Text = nowPlayer.ToString() + "플레이어가 승리했습니다.";
+                playing = false;
+                playButton.Text = "게임시작";
+            }
+            else
+            {
+                nowPlayer = ((nowPlayer == Horse.BLACK) ? Horse.WHITE : Horse.BLACK);
+                status.Text = nowPlayer.ToString() + "플레이어의 차례입니다.";
             }
         }
 
@@ -62,6 +114,22 @@ namespace Client
             {
                 gp.DrawLine(p, rectSize / 2, i, rectSize * edgeCount - rectSize / 2, i);
                 gp.DrawLine(p, i, rectSize / 2, i, rectSize * edgeCount - rectSize / 2);
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (!playing)
+            {
+                refresh(); // 화면 초기화
+                playing = true; // 게임을 시작중
+                playButton.Text = "재시작"; // 언제든 재시작 가능
+                status.Text = nowPlayer.ToString() + " 플레이어의 차례입니다.";
+            }
+            else
+            {
+                refresh();
+                status.Text = "게임이 재시작되었습니다.";
             }
         }
     }
